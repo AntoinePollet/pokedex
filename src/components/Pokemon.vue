2163b49b1e0ca5631e07a31a167af17614277925
AntoineP <pollet.antoine.alexis@gmail.com>
2022-09-25 20:38:25 +0200

    Renamed variables

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,8 +1,8 @@
 <script lang="ts">
 import { defineComponent, type PropType } from "vue";
 import type { PokemonBaseResult } from "@/types";
-import { pokemonSprite, firstUppercase } from "@/composables/pokemonUtilities"
-import PokemonTypes from "@/components/PokemonTypes.vue";
+import { pokemonSprite, firstUppercase } from "@/utilities/pokemonUtilities"
+import PokemonTypes from "@/components/PokemonType.vue";
 
 export default defineComponent({
     name: 'Pokemon',
@@ -29,7 +29,7 @@ export default defineComponent({
 <template>
     <div class="flex flex-col items-center border rounded-xl cursor-pointer">
         <div class="bg-[#F2F2F2] w-full rounded-t-xl py-5">
-            <img class="w-1/2 m-auto" :src="pokemonSprite(pokemon.id) ? pokemonSprite(pokemon.id) : '../assets/not-found.png'" :alt="pokemon.name"/>
+            <img class="w-1/2 m-auto" :src="pokemonSprite(pokemon.id)" :alt="pokemon.name"/>
             <div class="flex justify-center gap-3 flex-wrap">
                 <pokemon-types v-for="type in pokemon.pokemon_v2_pokemontypes" :type="type" />
             </div>
