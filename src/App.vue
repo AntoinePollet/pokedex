4df2169578f4ea43e9d764bb8dcb6b4e38142264
AntoineP <pollet.antoine.alexis@gmail.com>
2023-01-08 21:27:35 +0100

    Responsive for mobile

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <Header />
+    <Header @toggleMenu="toggleMenu" />
     <div class="pt-[64px] dark:bg-dark-1 overflow-auto dark:text-white" style="height: 100vh;">
         <div class="w-[80%] m-auto">
             <router-view></router-view>
@@ -9,7 +9,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import { createToast } from "mosha-vue-toastify";
 import { usePokedexStore } from "@/stores/pokedex";
 import localStorage from "@/composables/localStorage";
@@ -21,8 +21,12 @@ export default defineComponent({
         const pokedexStore = usePokedexStore();
         const { getFilteredPokemons } = pokedexStore;
 
+        const menuOpen = ref<boolean>(false);
+
         const storage = localStorage();
         const { filters } = storage;
+
+        const toggleMenu = (bool: boolean) => menuOpen.value = bool;
 
         onMounted(async () => {
             try {
@@ -31,6 +35,8 @@ export default defineComponent({
                 createToast('Error while fetching data', { type: "danger", position: "bottom-right" });
             }
         });
+
+        return { toggleMenu, menuOpen }
     }
 })
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-[64px] w-full bg-amber-300 dark:bg-dark-1 border-b border-solid border-dark-3 fixed z-50">
-        <div class="flex items-center h-full italic leading-loose text-lg relative w-full">
+        <div class="hidden sm:flex items-center h-full italic leading-loose text-lg relative w-full">
             <div class="flex gap-x-3 absolute left-1/2 top-1/2" style="transform: translate(-50%, -50%);">
                 <router-link class="hover:text-amber-800 font-bold dark:text-white dark:hover:text-amber-300" to="/">Pokedex</router-link>
                 <div class="flex items-center gap-x-1">
@@ -21,6 +21,31 @@
                 <generate-team-button />
             </div>
         </div>
+        <div class="sm:hidden flex justify-start items-center h-full pl-3">
+            <span @click="toggleMenu" class="material-symbols-outlined cursor-pointer dark:text-white" style="font-size: xxx-large">
+                menu
+            </span>
+        </div>
+
+        <div v-if="isMenuOpen">
+            <div class="sm:hidden h-screen w-screen bg-[#F2F2F2] dark:bg-dark-2 z-50 fixed p-4">
+                <div class="flex flex-col items-center gap-y-3">
+                    <router-link class="hover:text-amber-800 font-bold dark:text-white dark:hover:text-amber-300" to="/" @click="isMenuOpen = false">Pokedex</router-link>
+                    <router-link class="hover:text-amber-800 font-bold dark:text-white dark:hover:text-amber-300" to="/team" @click="isMenuOpen = false">Team</router-link>
+
+                    <div class="flex items-center">
+                        <span v-if="isDark" @click="toggleDark()" class="material-symbols-outlined cursor-pointer text-white">
+                            light_mode
+                        </span>
+                        <span v-else @click="toggleDark()" class="material-symbols-outlined cursor-pointer">
+                            dark_mode
+                        </span>
+                    </div>
+                    <clear-team-button />
+                    <generate-team-button />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -36,17 +61,21 @@ import Badge from "@/components/Badge.vue";
 import { useTeamStore } from "@/stores/team";
 import { storeToRefs } from "pinia";
 import { useToggle, useDark } from "@vueuse/core";
+import { ref, defineComponent } from "vue";
 
-export default {
+export default defineComponent({
     components: { GenerateTeamButton, ClearTeamButton, Badge },
-    setup() {
+    setup(props, { emit }) {
         const teamStore = useTeamStore();
         const { teamSize } = storeToRefs(teamStore);
 
+        const isMenuOpen = ref<boolean>(false);
         const isDark = useDark();
         const toggleDark = useToggle(isDark);
 
-        return { teamSize, toggleDark, isDark }
+        const toggleMenu = () => isMenuOpen.value = !isMenuOpen.value;
+
+        return { teamSize, toggleDark, isDark, toggleMenu, isMenuOpen }
     }
-}
+})
 </script>
