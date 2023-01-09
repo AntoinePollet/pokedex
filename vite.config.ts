c3315d5851a767acc374069aeeda68af34956db0
Antoine Pollet <pollet.antoine.alexis@gmail.com>
2023-01-09 09:39:06 +0100

    PWA minimal requirements

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,36 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue(), 
+    VitePWA({ 
+      registerType: 'autoUpdate' ,
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Pokedex',
+        short_name: 'Pokedex',
+        description: 'Pokedex finder',
+        theme_color: '#f2f2f2',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
